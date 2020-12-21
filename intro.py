fed06419e81d6488b5780694650f40f44d4be8ef
Vadim <shiva173@rambler.ru>
2020-12-21 18:47:10 +0300

    add new file

---
+++ intro.py
@@ -0,0 +1,23 @@
+# Task 1
+# user_number = int(input("Введи число: "))
+# print(user_number + 2)
+
+
+# Task 2
+# number=int(input('Введите число, которое будет больше 0, но меньше 10: '))
+#
+# while not (0 < number < 10):
+#     number = int(input('Введите число, которое больше нуля, но меньше 10: '))
+#
+# number **= 2
+# print(number)
+
+
+# Task 3
+name=input("Ваше имя: ")
+last_name=input("Ваша фамилия: ")
+age=int(input("Ваш возраст: "))
+weight=int(input("Ваш вес: "))
+
+
+
